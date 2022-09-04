af12f586df27bb3d5e9883707d430f8437db4e8d
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-09-03 20:56:42 -0400

    add codewars solution

---
+++ 8kyu/is-it-even.js
@@ -0,0 +1,19 @@
+// In this Kata we are passing a number (n) into a function.
+// Your code will determine if the number passed is even (or not).
+// The function needs to return either a true or false.
+// Numbers may be positive or negative, integers or floats.
+    function testEven(n){
+        return n % 2 === 0
+    }
+
+/*
+Create a function that determines if the inputs is even 
+Returning a boolean, true if the number is even otherwise false 
+Example 
+    0 => true
+    1 => false 
+    -4 => true
+Create a function testEven 
+One param n => number 
+Use % to check if the number is even
+*/
