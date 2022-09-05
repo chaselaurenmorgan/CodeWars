bd76f61fbf430212ec24eddafd43a98332be5e2f
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-09-04 20:11:28 -0400

    add codewars solution

---
+++ 8kyu/squaring-an-argument.js
@@ -0,0 +1,14 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+const square = number => number * number
+
+/*
+Create a function that squares the input 
+Returning a number, the number will be the input squared 
+Example 
+    2 => 4
+    3 => 9 
+    10 => 100 
+Create an arrow function square that takes one param  and return the  input multiplied by itself 
+*/
+
