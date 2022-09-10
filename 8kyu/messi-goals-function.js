fe76e37737efe70345e8b15519419e88ba06e49c
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-09-10 15:56:13 -0400

    add codewars solution

---
+++ 8kyu/messi-goals-function.js
@@ -0,0 +1,26 @@
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    
+    
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
+
+/*
+Create a function that that calculates the total goals from the three leagues. Function takes three parameters, all three will be numbers 
+Returning a number, which is the total of the three leagues 
+Example 
+    5, 10, 2  -->  17
+Create a function goals that sums up the following three params 
+ laLigaGoals
+ copaDelReyGoals
+ championsLeagueGoals
+
+*/
