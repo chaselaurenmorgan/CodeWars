9932b8b06f132b9e6b16aeb015db21c319a06b2d
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-09-20 19:31:36 -0400

    add codewars solution

---
+++ 8kyu/volume-of-cuboid.js
@@ -0,0 +1,15 @@
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+function getVolumeOfCuboid(length, width, height) {
+    return length * width * height
+  }
+/*
+Create a function that calculates the volume of a cuboid with three values 
+Returning a number, the calculationo of the volume 
+Example 
+    (1,2,2),  4
+    6,2,5), 60
+Create a function  getVolumeOfCuboid
+getVolumeOfCuboid takes three params (length,width,height)
+Calcution of Volume 
+    l * w * h 
+*/
