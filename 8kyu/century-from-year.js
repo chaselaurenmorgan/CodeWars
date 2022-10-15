4f0d19339fda93b04b98b847866edcd5c2a2cd6a
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-10-14 20:46:31 -0400

    add codewars solution

---
+++ 8kyu/century-from-year.js
@@ -0,0 +1,16 @@
+// Given a year, return the century it is in.
+
+    const century = year => Math.ceil(year/100)
+
+/*
+Create a function that is give a year and return the century that it is in
+Return a number, that represents the century
+Example
+    1705 --> 18
+    1900 --> 19
+    1601 --> 17
+    2000 --> 20
+Create a function century, that takes one argument of year 
+To get the century divide the given parameter by 100 and round up 
+Example => Math.ceil(1705/100)
+*/
