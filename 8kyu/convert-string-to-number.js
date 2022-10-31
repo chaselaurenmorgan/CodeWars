7671d46e324aa46515f8990caeb1d30f44e96841
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-10-31 14:48:18 -0400

    add codewars solution

---
+++ 8kyu/convert-string-to-number.js
@@ -0,0 +1,24 @@
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+
+const convertStringToNumber = string => +string
+
+console.log(convertStringToNumber('1234'))
+console.log(convertStringToNumber('-7'))
+console.log(convertStringToNumber('1405'))
+
+
+
+
+
+
+
+/*
+1.Create a function that takes a string as a parameter and converts it into a number 
+2. return a number from the parameter of strings
+3. '1234' => 1234 , '-7' => -7, '1405' => 1405
+4. Use a unirary + to automatically convert to a number 
+    - use Number()
+    -parseInt()
+*/
+
