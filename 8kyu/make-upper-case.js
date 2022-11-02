f617a5f1a5f5f516f0ac5fbe4b9d5e326596bed3
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-11-02 15:24:32 -0400

    add codewars solution

---
+++ 8kyu/make-upper-case.js
@@ -0,0 +1,13 @@
+// Write a function which converts the input string to uppercase.
+
+const makeUpperCase = str => str.toUpperCase()
+
+
+
+/*
+Create function that turns a string into all UpperCase 
+Returning a string in all UpperCase 
+'chase' => CHASE 'HELLO' => 'HELLO'
+1. create a funtion makeUpperCase with a parameter of string 
+2. change the string to all UpperCase use the toUpperCase()
+*/
