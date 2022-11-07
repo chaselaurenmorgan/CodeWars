71ceb302a583e2c930b779c1ed1ce7093bb281df
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-11-07 06:11:25 -0500

    solve 8kyu's

---
+++ 8kyu/even-or-odd.js
@@ -0,0 +1,14 @@
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number){
+    return number % 2 === 0 ? 'Even' : 'Odd'
+
+} 
+
+
+
+// create a fuction that takes a number as a parameter and returns 'even' if the number is even or returns 'odd' for odd numbers
+//returning 'Even' if the number is even or returning the 'Odd' if the number is odd 
+// (2,'Even'), (3,'Odd') (-6, 'Even')
+//to test if a number is even  => % 
---
+++ 8kyu/opposite-number.js
@@ -0,0 +1,14 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+
+function opposite(number) {
+    
+    return -(number)
+
+  }
+
+
+  //created a function that takes number and returns the opposite
+  //returning the opposite of the parameter
+  //(1,-1) , (14,-14) , (-34,34)
+  // to change the number to opposite can use a negative 
