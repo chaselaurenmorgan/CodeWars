0bed2d3edd44bfde565723322e8bee4b8132728a
Chase L. Morgan <chasemorgan15@yahoo.com>
2022-11-29 07:09:27 -0500

    add codewars solution

---
+++ 8kyu/check-for-factor.js
@@ -0,0 +1,22 @@
+// This function should test if the factor is a factor of base.
+// Return true if it is a factor or false if it is not.
+
+function checkForFactor(base,factor){
+    return base % factor === 0 
+}
+
+/*
+Create a function that test whether factor is divisible by the base and return a boolean 
+Returning a boolean, true if the base is divisible by the factor and false otherwise 
+Example 
+    10,2 => true
+    63,7 => true
+    9,2 => false
+Create a function checkForFactor 
+2 params 
+    base(number)
+    factor(number)
+Check if the base is divisible by the factor, use % 
+base % factor === 0 
+*/
+
