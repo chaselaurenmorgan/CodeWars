b788f51d58b24309f60f7bb1755d7e7bf7611274
Chase Morgan <chasemorgan15@yahoo.com>
2022-12-11 18:22:34 -0500

    added codewars solution

---
+++ 8kyu/you-only-need-one.js
@@ -0,0 +1,21 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not
+
+
+function check(a, x) {
+    // your code here
+    return a.includes(x)
+  }
+
+
+
+/*
+Create a function that check if an array contains a value.
+Two params, arr and value(number or string)
+Returning a boolean, true if the value is inside of the array and false otherwise
+Example  [1,2,3,4], 4 => true
+Use the includes method on an array to return a boolean t
+*/
