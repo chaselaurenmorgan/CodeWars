7c907826e42201fc6a8b506c2d4b1a06b6a493a2
Chase Morgan <chasemorgan15@yahoo.com>
2023-01-06 07:09:18 -0500

    added codewar solution

---
+++ 8kyu/returning-strings.js
@@ -0,0 +1,17 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+    const greet = name => `Hello, ${name} how are you doing today?`
+
+    console.log(greet('Ryan'))
+
+/*
+Create a function that returns a greeting statement with the argument. 
+One param which will be a string 
+Returning a string with the param
+Example => 'Ryan' => 'Hello, Ryan how are you doing today?'
+How do we add the param into the string 
+    - concatenation 
+    - interpolation with temperal literal 
+*/
