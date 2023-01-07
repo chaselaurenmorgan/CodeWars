419f801e09bc26fea46adb055e703e1e81138882
Chase Morgan <chasemorgan15@yahoo.com>
2023-01-07 07:16:06 -0500

    added codewar solution

---
+++ 8kyu/boolean-to-string.js
@@ -0,0 +1,22 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+
+function booleaToString(b){
+    return String(b)
+}
+
+
+
+/*
+Create a function that is given a boolean value (true/false) and convert it into a string
+One param which will be a boolean 
+Returning a string, string version of the given boolean 
+Example true => 'true',  false => 'false'
+How would we change anything to a string? 
+ - concat with + ''
+ - String()
+ - toString()
+ - use ternary/ if else statement 
+*/
