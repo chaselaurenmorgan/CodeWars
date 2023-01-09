1c6119aa4bc720de310812ea524b1ac49822f921
Chase Morgan <chasemorgan15@yahoo.com>
2023-01-09 15:09:51 -0500

    added codewar solution

---
+++ 8kyu/under-pressure.js
@@ -0,0 +1,15 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
+
+/*
+Create a function that take an interger and double it
+Functions takes one param which by an interger 
+Returning a number that will be the integer multiplied by 2
+Example 2 => 4 
+Multiply the interger by 2 
+*/
+
