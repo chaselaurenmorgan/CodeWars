779cb4669fa71744cb9f1be248a2a40d7d8f0c47
Chase Morgan <chasemorgan15@yahoo.com>
2023-01-12 15:51:07 -0500

    added codewar solution

---
+++ 8kyu/lost-without-map.js
@@ -0,0 +1,15 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+function maps(x){
+    return x.map(x => x * 2)
+  }
+
+/*
+Create a fuction that takes an array of intergers and returns a new array with each element in the array doubled
+One param, array of integers 
+Returning a new array with the integer values doubled 
+Example => [1, 2, 3] --> [2, 4, 6]
+Use the map method to iterate through the array and multiply each element by two
+*/
