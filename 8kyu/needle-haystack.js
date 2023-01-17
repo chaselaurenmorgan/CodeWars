97918aa64faddfb6c98615025b3700c5cf523f2c
Chase Morgan <chasemorgan15@yahoo.com>
2023-01-17 16:47:48 -0500

    added codewar solution

---
+++ 8kyu/needle-haystack.js
@@ -0,0 +1,20 @@
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+function findNeedle(haystack) {
+    // your code here
+    return `found the needle at position ${haystack.indexOf('needle')}`
+  }
+
+/*
+Create a function that takes finds the 'needle' in the array and return it's index withing a string
+One param an array of strings 
+return "found the needle at position " plus the index it found the needle
+Use indexOf method to return the index positon of the 'needle' within the array 
+*/
+
