6115c5232d51a4b143bcde376701ad407c19c278
Chase Morgan <chasemorgan15@yahoo.com>
2023-01-21 07:44:41 -0500

    added codewars solution

---
+++ 8kyu/simple-multiplication.js
@@ -0,0 +1,15 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+
+function simpleMultiplication(number) {
+    // your code........
+  return number % 2 === 0 ? number * 8 : number * 9
+}
+
+
+/*
+Creata function that multiplies a number by eight only if it an even numbers otherwise multiply the number by nine.
+One param a number 
+Returning a number, number * 8 if even, otherwise the number * 9 
+Use a ternary to solve
+*/
