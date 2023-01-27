2944c2573f26d1975b1b2bcf7e508466a9ac4f55
Chase Morgan <chasemorgan15@yahoo.com>
2023-01-27 10:17:54 -0500

    added codewars solution

---
+++ 8kyu/max-min-values.js
@@ -0,0 +1,24 @@
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+
+
+const min = function(list){
+    
+    return Math.min(...list);
+}
+
+const max = function(list){
+    
+    return Math.max(...list)
+}
+
+
+
+
+
+/*
+Create two separate functions one to return the min number in and array and the other array will return the max number in the array
+Only one param which be an array of numbers 
+Returning the min and max numbers in the array 
+Use the math objec of min and max to find the min and max of the array by using the spread operator
+*/
